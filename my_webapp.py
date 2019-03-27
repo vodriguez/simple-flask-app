6c878f796d861396727ff2f5d835e36ca753e6bd
vodriguez <agarciarod@gmail.com>
2019-03-27 20:21:47 +0000

    my first commit

--- my_webapp.py
+++ my_webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import logging
 
 flask_app = Flask(__name__)
@@ -24,9 +24,12 @@ logger.addHandler(fh)
 def homepage():
     return "Hello World"
 
+@flask_app.route('/<name>')
+def hello_someone(name):
+    return render_template("hello.html", name=name.title())
+
 
 logger.info('STARTING APP, TRY IT OUT!!!')
 
 if __name__ == '__main__':
     flask_app.run(debug=True, use_reloader=True)
-
